8cc898827e99da5e13f5cb5ea784e40dc3fc2d04
AjitsinghKamal <ajitskamal@gmail.com>
2021-06-15 19:46:11 +0530

    db: migrate to postgresql

--- pages/api/tags/index.ts
+++ pages/api/tags/index.ts
@@ -4,7 +4,6 @@ import prisma from "lib/prisma";
 // GET /api/tags
 
 export default async function handle(_: NextApiRequest, res: NextApiResponse) {
-	console.log("pr");
 	const tags = await prisma.tags.findMany();
 	res.json(tags);
 }
