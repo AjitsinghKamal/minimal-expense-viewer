5a352fc3ec7485d0a99ad373c18ca3678a13b6dd
AjitsinghKamal <ajitskamal@gmail.com>
2021-06-15 12:42:49 +0530

    chore: update env url

--- http/expense.ts
+++ http/expense.ts
@@ -1,17 +1,17 @@
 export const fetchTags = () => {
-	return fetch(`${process.env.APP_PATH}/api/tags`, {
+	return fetch(`${process.env.VERCEL_URL}/api/tags`, {
 		headers: { "Content-Type": "application/json" },
 	});
 };
 
 export const fetchTeams = () => {
-	return fetch(`${process.env.APP_PATH}/api/teams`, {
+	return fetch(`${process.env.VERCEL_URL}/api/teams`, {
 		headers: { "Content-Type": "application/json" },
 	});
 };
 
 export const fetchTransactions = () => {
-	return fetch(`${process.env.APP_PATH}/api/transactions`, {
+	return fetch(`${process.env.VERCEL_URL}/api/transactions`, {
 		headers: { "Content-Type": "application/json" },
 	});
 };
